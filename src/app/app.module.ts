5b23a3ec1b2f4e5ccf545b242aba9ff25c7d8503
msimonz <simon.marquezok@gmail.com>
2025-04-22 14:48:28 -0500

    Corrección de error fusion de ramas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,8 +19,7 @@ import { ClienteDetallesComponent } from './clientes/cliente-detalles/cliente-de
 import { ClienteFormComponent } from './clientes/cliente-form/cliente-form.component';
 import { ClienteUpdateComponent } from './clientes/cliente-update/cliente-update.component';
 import { ClienteLoginComponent } from './auth/cliente-login/cliente-login.component';
-import { DashboardEstadisticasComponent } from './dashboard/dashboard-estadisticas/dashboard-estadisticas.component';
-import { ChartsModule } from 'ng2-charts';
+
 
 
 
@@ -41,7 +40,6 @@ import { ChartsModule } from 'ng2-charts';
     ClienteFormComponent,
     ClienteUpdateComponent,
     ClienteLoginComponent,
-    DashboardEstadisticasComponent
   ],
   imports: [
     BrowserModule,
@@ -49,7 +47,6 @@ import { ChartsModule } from 'ng2-charts';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
